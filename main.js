0ad9b150ce8b6df75f50caceaceed10a5e63755a
Jakub Pociask <jakub.pociask@gmail.com>
2020-05-18 23:15:08 +0200

    header styling; accouncement section added with js injection

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Przybywaj!");
+
+
+const ilosc = '2';
+const gratis = 'tester'
+
+const nowosc = document.querySelector('.announcement--injection')
+
+nowosc.innerHTML = `Zamów już teraz sztuk: ${ilosc} --- dostaniesz ${gratis} gratis!`
